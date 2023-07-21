e0df5c38dfb54468a5bb452ede250e0073c2eceb
EnesArslan8 <enes8arslan@gmail.com>
2023-07-21 14:27:37 +0300

    Header component created

---
+++ src/Components/Header.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { BsFillSunFill, BsMoonStarsFill } from "react-icons/bs";
+
+function Header() {
+  const [isDark, setIsDark] = useState(false);
+
+  const themeDark = () => {
+    setIsDark(true);
+  };
+  const themeLight = () => {
+    setIsDark(false);
+  };
+
+  console.log(isDark);
+
+  return (
+    <div>
+      <span>TODO</span>
+      {isDark == false ? (
+        <BsMoonStarsFill onClick={themeDark} className="dark"></BsMoonStarsFill>
+      ) : (
+        <BsFillSunFill onClick={themeLight} className="light"></BsFillSunFill>
+      )}
+    </div>
+  );
+}
+
+export default Header;
